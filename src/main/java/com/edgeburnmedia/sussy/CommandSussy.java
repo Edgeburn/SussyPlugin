2cfc74a942f78cecc75d03fff055ec8cf3dd6089
Connor Watson <connorwatson002@outlook.com>
2021-09-04 00:28:40 -0400

    Added adventure mode check & moved showing title to separate function

--- src/main/java/com/edgeburnmedia/sussy/CommandSussy.java
+++ src/main/java/com/edgeburnmedia/sussy/CommandSussy.java
@@ -24,7 +24,6 @@ public class CommandSussy implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player playerToTeleport;
         Location currentPlayerLocation;
-        String subtitle;
 
         if (args.length == 0 && sender instanceof Player) { // command is being executed by a player with no arguments, so teleport themselves
             playerToTeleport = (Player) sender;
@@ -45,24 +44,38 @@ public class CommandSussy implements CommandExecutor {
 
         currentPlayerLocation = playerToTeleport.getLocation(); // get the current location of the player to be teleported
         Location locationToTeleportTo = new Location(playerToTeleport.getWorld(), currentPlayerLocation.getX(), VOID_LOCATION, currentPlayerLocation.getZ()); // set the location to which the player will be teleported, which is ~ -300 ~
-        playerToTeleport.setGameMode(GameMode.SURVIVAL);
+
+        if (!(playerToTeleport.getGameMode() == GameMode.ADVENTURE)) { // we don't want to change the player's gamemode to survival if they're in adventure because it could break things
+            playerToTeleport.setGameMode(GameMode.SURVIVAL);
+        }
+
         playerToTeleport.teleport(locationToTeleportTo);
+        showTitle(playerToTeleport, sender);
+        sender.sendMessage(playerToTeleport.getDisplayName() + " was ejected!");
+        getLogger().info(sender.getName() + " ejected " + playerToTeleport.getDisplayName());
+        return true;
+    }
+
+    public void showTitle (Player playerToTeleport, CommandSender sender) {
+        String title;
+        String subtitle;
+
         if (showEjectorName) {
             subtitle = "They were ejected by " + sender.getName();
         } else {
             subtitle = "They were ejected";
         }
 
+        title = playerToTeleport.getDisplayName() + " was the sussy Imposter!";
+
         if (broadcastEjectToEveryone) {
             for (Player player : getOnlinePlayers()) { // send everyone on the server the message that the player in ejected was the sussy imposter
-                player.sendTitle(playerToTeleport.getDisplayName() + " was the sussy Imposter!", subtitle, 1, 70, 10);
+                player.sendTitle(title, subtitle, 1, 70, 10);
             }
         } else {
-            playerToTeleport.sendTitle(playerToTeleport.getDisplayName() + " was the sussy Imposter!", subtitle, 1, 70, 10);
-            ((Player) sender).sendTitle(playerToTeleport.getDisplayName() + " was the sussy Imposter!", subtitle, 1, 70, 10);
+            playerToTeleport.sendTitle(title, subtitle, 1, 70, 10);
+            ((Player) sender).sendTitle(title, subtitle, 1, 70, 10);
         }
-        sender.sendMessage(playerToTeleport.getDisplayName() + " was ejected!");
-        getLogger().info(sender.getName() + " ejected " + playerToTeleport.getDisplayName());
-        return true;
+
     }
 }
