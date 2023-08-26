2581d7a151bcad033f28b3dd57e5e68a74e10896
Edgeburn <edgeburn02@mailredirect.edgeburnmedia.com>
2023-08-26 18:14:58 -0400

    Add PvPManager as a compileOnly dependency and untag players in untagCombat.

--- src/main/java/com/edgeburnmedia/sussy/Sussy.java
+++ src/main/java/com/edgeburnmedia/sussy/Sussy.java
@@ -1,6 +1,10 @@
 package com.edgeburnmedia.sussy;
 
+import me.NoChance.PvPManager.Managers.PlayerHandler;
+import me.NoChance.PvPManager.PvPManager;
+import me.NoChance.PvPManager.PvPlayer;
 import org.bstats.bukkit.Metrics;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
@@ -50,20 +54,27 @@ public final class Sussy extends JavaPlugin {
 	 * @param ejector          The player who ejected the player.
 	 */
 	public static void sussyPlayer(Player playerToTeleport, CommandSender ejector) {
-		// Ensure that no matter what the world's minimum height is (should it be set via a datapack) it will always teleport 300 blocks below bedrock.
-		double voidLocation = playerToTeleport.getWorld().getMinHeight() - 300.0;
-
-		Location currentPlayerLocation = playerToTeleport.getLocation(); // get the current location of the player to be teleported
-		Location locationToTeleportTo = new Location(playerToTeleport.getWorld(), currentPlayerLocation.getX(), voidLocation, currentPlayerLocation.getZ()); // set the location to which the player will be teleported, which is ~ -300 ~
+		Location locationToTeleportTo = getSussyLocation(playerToTeleport);
 
 		if (!(playerToTeleport.getGameMode() == GameMode.ADVENTURE)) { // we don't want to change the player's gamemode to survival if they're in adventure because it could break things
 			playerToTeleport.setGameMode(GameMode.SURVIVAL);
 		}
 
+		untagCombat(playerToTeleport);
+
 		playerToTeleport.teleport(locationToTeleportTo);
 		showTitle(playerToTeleport, ejector);
 
 		getInstance().getLogger().info(ejector.getName() + " ejected " + playerToTeleport.getDisplayName());
+	}
+
+	private static Location getSussyLocation(Player playerToTeleport) {
+		// Ensure that no matter what the world's minimum height is (should it be set via a datapack) it will always teleport 300 blocks below bedrock.
+		double voidLocation = playerToTeleport.getWorld().getMinHeight() - 300.0;
+
+		Location currentPlayerLocation = playerToTeleport.getLocation(); // get the current location of the player to be teleported
+		Location locationToTeleportTo = new Location(playerToTeleport.getWorld(), currentPlayerLocation.getX(), voidLocation, currentPlayerLocation.getZ()); // set the location to which the player will be teleported, which is ~ -300 ~
+		return locationToTeleportTo;
 	}
 
 	/**
@@ -107,5 +118,22 @@ public final class Sussy extends JavaPlugin {
 
 	public static boolean supportsPackets() {
 		return true;
+	}
+
+	static void untagCombat(Player player) {
+		try {
+			PvPManager pvpmanager = null;
+			if (Bukkit.getPluginManager().isPluginEnabled("PvPManager")) {
+				pvpmanager = (PvPManager) Bukkit.getPluginManager().getPlugin("PvPManager");
+			}
+
+			PlayerHandler playerHandler = pvpmanager.getPlayerHandler();
+
+			PvPlayer pvplayer = playerHandler.get(player);
+
+			pvplayer.unTag();
+		} catch (NoClassDefFoundError e) {
+			// ignore
+		}
 	}
 }
